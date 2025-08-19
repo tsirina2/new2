705288aff04c0a56f0b9bfdd84fe73168ed7d55f
tsirina2 <tsirina2@comcast.net>
2025-08-19 12:14:28 -0400

    Update 235.py

--- 235.py
+++ 235.py
@@ -1,12 +1,13 @@
 array = [2, 9, 11, 12, 14, 7, 12, 218]
 def find():
-    max = array[7]
+    max = array[8]
     for element in array:
         if element > max:
             max = element
     return max
 find()
 print(find())
+
 
 
 
