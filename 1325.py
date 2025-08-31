c725d72b5db9c1e72502075747cbcc117ff7d6db
tsirina2 <tsirina2@comcast.net>
2025-08-31 09:21:54 -0400

    Add files via upload

---
+++ 1325.py
@@ -0,0 +1,26 @@
+array = [2, 9, 11, 12, 14,300,0, 7, 12, 218]
+array1 =[]
+def find(array):
+    value_max = array[0]
+    for element in array:
+        if element > value_max:
+            value_max = element
+def append(array1):
+    value_max = array1[0]
+    for element in array1:
+        if element > value_max:
+            array[i] = array1[i+1]
+        return array1
+
+    print(array1)
+
+
+
+
+
+
+
+
+
+
+
