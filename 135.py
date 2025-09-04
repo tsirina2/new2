249eaa13745a21557d6026279c9748a1ad960cca
tsirina2 <tsirina2@comcast.net>
2025-09-04 10:52:48 -0400

    Add files via upload

--- 135.py
+++ 135.py
@@ -1,26 +1,13 @@
-array = [2, 9, 11, 12, 14,300,0, 7, 12, 218]
-array1 =[]
-def find(array):
-    value_max = array[0]
-    for element in array:
-        if element > value_max:
-            value_max = element
-def append(array1):
-    value_max = array1[0]
-    for element in array1:
-        if element > value_max:
-            array[i] = array1[i+1]
-        return array1
-
-    print(array1)
-
-
-
-
-
-
-
-
-
-
-
+original_array = [300,32, 9, 11, 12, 14, 7, 12, 218]
+result = []
+def find(original_array):
+    max_value = original_array[0]
+    for element in original_array:
+        if element > max_value:
+            max_value = element
+        return max_value
+    while len(original_array) > 0:
+        max_value = find(original_array)
+        result.append(max_value)
+        original_array.remove(max_value)
+    print(result.append(max_value))
