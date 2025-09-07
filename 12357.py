dacef77df15a64bc58c6a5e63bf6fd824310cebd
tsirina2 <tsirina2@comcast.net>
2025-09-07 11:38:27 -0400

    Add files via upload

---
+++ 12357.py
@@ -0,0 +1,16 @@
+from nt import remove
+
+original_array = [300,32, 9, 11, 12, 14, 7, 12, 218]
+result = []
+def find(max_value):
+    max_value = original_array[element]
+    for element in original_array:
+        if element > max_value:
+            max_value = element
+            max_value = result[element-1]
+            remove(original_array.max_value)
+    while len(original_array) > 0:
+        append(result[element])
+
+    return find(max_value)
+    print(result)
