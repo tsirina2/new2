42c074ad536bd7be3f5dbe63193d1a8fe54c018a
tsirina2 <tsirina2@comcast.net>
2025-09-10 10:50:02 -0400

    Add files via upload

---
+++ 175.py
@@ -0,0 +1,12 @@
+original_array = [300, 32, 9, 11, 12, 14, 7, 12, 218]
+result = []
+while len(original_array) > 0:
+    remove(original_array[max_value(original_array)])
+    append(result[max_value])
+    def find(original_array):
+        max_val = original_array[0]
+        for item in original_array:
+            if item > max_val:
+                max_val = item
+                return_result = max_val
+
