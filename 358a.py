db9e788b350528e1470019e811740ab7caca5db4
tsirina2 <tsirina2@comcast.net>
2025-09-18 08:49:37 -0400

    Add files via upload

---
+++ 358a.py
@@ -0,0 +1,16 @@
+def find(array):
+   max_val = array[0]
+   for item in array:
+      if item>max_val:
+         max_val = item
+   return max_val
+
+
+original_array = [300,32,9,11,12,14,7,12]
+result =[]
+while len(original_array)>0:
+   max_elmenent = find(original_array)
+   original_array.remove(max_elmenent)
+   result.append(max_elmenent)
+print(result)
+
